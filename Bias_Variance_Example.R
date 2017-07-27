8d51ec03bc6a617840ad49970db91122eec87b44
Matt Steele <Matt Steele>
2017-07-26 20:34:05 -0400

    Improved graph and added .png

--- Bias_Variance_Example.R
+++ Bias_Variance_Example.R
@@ -1,6 +1,6 @@
 #Create truth function
 f = function(x) {
- x ^ 3
+ x ^ 3 
 }
 
 #Function to generate data
@@ -19,31 +19,41 @@ y = data$y
 linear = lm(y ~x, data = data)
 poly2 = lm(y ~ poly(x, degree = 2), data = data)
 poly3 = lm(y ~ poly(x, degree = 3), data = data)
-spline = smooth.spline(x=x,y=y, spar = 0.2)
+spline = smooth.spline(x=x,y=y, spar = 0.3)
+
+#To save a higher resolution output, uncomment below and at the end of the script. The image will
+#be stored in your current working directory
+#png("Bias_Variance_Plot.png", width = 3700, height = 3500, res = 1000)
 
 #Set seed for random data, plot datapoints
 set.seed(100)
 plot(y ~ x, data = data)
 
 #New data to predict on 
-newdata = seq(from = -1, to = 1, by = 0.02)
+newdata = seq(from = -10, to = 10, by = 0.2)
 
 #Predict on newdata, plot as line on chart
 lines(newdata, predict(linear, newdata = data.frame(x = newdata)), 
-      col = "midnightblue", lwd = 4, lty = 1)
+      col = "midnightblue", lwd = 3, lty = 1)
+
+lines(newdata, predict(poly2, newdata = data.frame(x = newdata)),
+      col = "red", lwd = 3, lty = 1)
 
 lines(newdata, predict(poly3, newdata = data.frame(x = newdata)), 
-      col = "yellow2", lwd = 4, lty = 1)
+      col = "yellow2", lwd = 3, lty = 1)
 
 lines(predict(spline, newdata=data.frame(x = newdata)),
-      col = "grey40", lwd = 4, lty = 1)
+      col = "grey40", lwd = 3, lty = 1)
 
 #Plot ground truth using our defined function
-lines(newdata, f(newdata), col = "black", lwd = 7)
+lines(newdata, f(newdata), col = "black", lwd = 5)
 
 #Plot aesthetics
-legend('bottomright', c("y ~ x", "y ~ poly(x, 3)", "spline", "truth"), 
-       col = c("midnightblue", "yellow2", "grey20", "black"), lty = c(1, 1, 1, 1), 
-       lwd = 2, cex=.9,text.width=.3, bty='n', y.intersp = .5)
+legend('bottomright', c("linear", "poly ^ 2", "poly ^ 3", "spline", "truth"), 
+       col = c("midnightblue", "red", "yellow2", "grey20", "black"), lty = c(1, 1, 1, 1, 1), 
+       lwd = 3, cex=.8,text.width=.3, bty='n', y.intersp = .75)
 
 title("Model Flexibility and Bias-Variance", line = -2)
+
+#For a higher resolution output, uncomment below
+dev.off()
